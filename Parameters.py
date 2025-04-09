3deb81c054e0be6b8279ec96d305796aca34d405
Marilina Valatsou <mvalatsou@phys.ethz.ch>
2025-04-09 16:25:55 +0200

    Added proper incorporation of Helium in HHeH2O mixture

--- Parameters.py
+++ Parameters.py
@@ -9,22 +9,33 @@ class ModelParams:
         self.m_H            = 1.6735575e-24                     # Mass of hydrogen atom, g (also proton mass)
         self.m_O            = self.am_o * self.m_H              # Μass of oxygen atom, grams
         self.m_CO2          = 44 * self.m_H                     # Mass of CO2 molecule, grams
+
         # - Mean molecular weights for non-dissociated species
         self.mmw_H          = 1                                 # Mean molecular weight (H)
         self.mmw_HHe        = 2.35                              # Mean molecular weight (HHe)
         self.mmw_H2O        = 2 * self.am_h + self.am_o         # Mean molecular weight (water)
         self.mmw_HHe_10H2O  = 0.9 * self.mmw_HHe + 0.1 * self.mmw_H2O # Mean molecular weight (HHe & 10% water)
-        # - Mean molecular weights for fully dissociated species assuming 
-        # *strong* photoevaporation that fully dissociates the species
-        self.mmw_HHe_outflow = self.mmw_H                       # 1, full dissociation gives free H atoms (mmw = 1 per H atom)
+
+        # - Mean molecular weights for fully dissociated species assuming *strong* photoevaporation that fully dissociates the species
+        self.mmw_HHe_outflow = self.mmw_H                       # 1, ignores He, full dissociation gives free H atoms (mmw = 1 per H atom)
         self.mmw_H2O_outflow = (2 * self.am_h + self.am_o) / 3  # = (2+16) / 3 = 6, max mean molecular weight (H, H, and O) assuming full dissociation (H2O -> 2H + O)
-        # for mixture of HHe an H2O
-        self.X_HHe  = 0.9
-        self.X_H2O  = 0.1
-        self.N_HHe  = self.X_HHe / self.mmw_HHe_outflow         # = 0.9 / 1 = 0.9
-        self.N_H2O  = self.X_H2O / self.mmw_H2O_outflow         # = 0.1 / 6 ≈ 0.01667
-        self.N_tot  = self.N_HHe + self.N_H2O                   # total free particles per unit mass
-        self.mmw_HHe_10H2O_outflow = 1 / self.N_tot             # Max mean molecular weight (90% of H, H, 10% of H, H, and O) assuming full dissociation of H2 and water
+        # # - To properly include Helium and not ignore it:
+        # X_H         = 0.70
+        # X_He        = 0.30
+        # mmw_H_free  = self.mmw_H                                # 1 for free hydrogen
+        # mmw_He      = 4                                         # 4 for atomic helium
+        # N_H = X_H / mmw_H_free                                  # Number of free hydrogen atoms per unit mass
+        # N_He = X_He / mmw_He                                    # Number of free helium atoms per unit mass
+        # N_total_HHe = N_H + N_He
+        # self.mmw_HHe_outflow = 1 / N_total_HHe                  # effective mean molecular weight including helium. ~1.29 given X_H=0.70 and X_He=0.30.
+
+        # - For mixture of HHe an H2O
+        X_HHe   = 0.9
+        X_H2O   = 0.1
+        N_HHe   = X_HHe / self.mmw_HHe_outflow                  # = 0.9 / 1 = 0.9
+        N_H2O   = X_H2O / self.mmw_H2O_outflow                  # = 0.1 / 6 ≈ 0.01667
+        N_tot   = N_HHe + N_H2O                                 # total free particles per unit mass
+        self.mmw_HHe_10H2O_outflow = 1 / N_tot                  # Max mean molecular weight (90% of H, H, 10% of H, H, and O) assuming full dissociation of H2 and water
 
         self.k_b    = 1.380649e-16      # Boltzmann constant, erg K-1
         self.k_b_SI = 5.670374419e-8    # Boltzmann constant (SI) W m-2 K-1
@@ -40,7 +51,7 @@ class ModelParams:
         self.E_photon   = 20 * 1.6e-12  # photon energy
         self.FEUV       = 450.          # received EUV flux, ergs cm-2 s-1
         self.albedo     = 0.3           # albedo of planet
-        self.beta_planet= 0.75          # fraction of the planet's surface that re-emites radiation
+        self.beta_planet= 0.75          # fraction of the planet's surface that re-emits radiation
         self.epsilon    = 1.            # emissivity of planet
         self.sigma_EUV  = 1.89e-18      # EUV cross-section (of H? H2? similar for O), cm2
         self.alpha_rec  = 2.6e-13       # Recombination coefficient, cm3 s-1
